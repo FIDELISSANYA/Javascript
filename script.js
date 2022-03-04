19a4033d5b115f24db993b872a0bde77047d6d82
FIDELIS SANYA <fidelis.sanya@student.moringaschool.com>
2022-03-04 12:12:39 +0300

    craete day fuction

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 function submitForm(){
     alert("submit succesful");
     }
-    
+    function fuctionDay(){
+        const inpDay=document.getElementById("id");
+        if(!inpDay.checkValidity()) {
+            document.getElementById("demo").innerHTML=inpDay.validationMessage;
+        }
+        else{
+            document.getElementById("demo").innerHTML=InputAccepted; 
+        }
+    }
